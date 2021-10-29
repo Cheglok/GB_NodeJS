3f3e4e30c7bf5e437765adfb898465840c80d450
Cheglok <Kopytovmikhail@yandex.ru>
2021-10-29 22:51:05 +0500

    Quantity of streams equals to ip-addresser

--- lesson3/task1.js
+++ lesson3/task1.js
@@ -5,15 +5,16 @@ const INPUT_IP_ADDRESSES = process.argv.slice(2);
 const lineReader = require('readline').createInterface({
     input: fs.createReadStream(ACCESS_LOG, 'utf-8'),
 })
-INPUT_IP_ADDRESSES.forEach((address) => {
+let writeStreams = [];
 
+INPUT_IP_ADDRESSES.forEach((address) => {
+    writeStreams[address] = fs.createWriteStream(address + '.log',  { flags: 'a', encoding: 'utf8' });
 });
 
 lineReader.on('line', (line) => {
     INPUT_IP_ADDRESSES.forEach((address) =>{
         if(line.includes(address)) {
-            const writeStream = fs.createWriteStream(address + '.log',  { flags: 'a', encoding: 'utf8' });
-            writeStream.write(line + '\n');
+            writeStreams[address].write(line + '\n');
         }
     })
 })
