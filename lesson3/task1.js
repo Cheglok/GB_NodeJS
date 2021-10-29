5a1f158f56b0f4a8648840602848277d1a0743c7
Cheglok <Kopytovmikhail@yandex.ru>
2021-10-29 23:26:30 +0500

    add error handlers

--- lesson3/task1.js
+++ lesson3/task1.js
@@ -1,14 +1,18 @@
 const fs = require('fs');
-const ACCESS_LOG = './lesson3/test.log';
+const ACCESS_LOG = './lesson3/access.log';
 const INPUT_IP_ADDRESSES = process.argv.slice(2);
 
+const reader = fs.createReadStream(ACCESS_LOG, 'utf-8')
 const lineReader = require('readline').createInterface({
-    input: fs.createReadStream(ACCESS_LOG, 'utf-8'),
+    input: reader,
 })
+reader.on('error', (err) => console.log('hello' + err));
+
 let writeStreams = [];
 
 INPUT_IP_ADDRESSES.forEach((address) => {
     writeStreams[address] = fs.createWriteStream(address + '.log',  { flags: 'a', encoding: 'utf8' });
+    writeStreams[address].on('error', (err) => console.log(err));
 });
 
 lineReader.on('line', (line) => {
@@ -18,3 +22,6 @@ lineReader.on('line', (line) => {
         }
     })
 })
+
+lineReader.on('end', () => console.log("Завершено чтение файла"));
+lineReader.on('error', (err) => console.log('err'));
