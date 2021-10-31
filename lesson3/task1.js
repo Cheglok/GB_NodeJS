fc75f43041231506801e94ff3af4e6c4495324c7
Cheglok <Kopytovmikhail@yandex.ru>
2021-10-31 16:29:09 +0500

    Change end event to close for readline, now it works

--- lesson3/task1.js
+++ lesson3/task1.js
@@ -1,12 +1,12 @@
 const fs = require('fs');
-const ACCESS_LOG = './lesson3/access.log';
+const ACCESS_LOG = './lesson3/test.log';
 const INPUT_IP_ADDRESSES = process.argv.slice(2);
 
 const reader = fs.createReadStream(ACCESS_LOG, 'utf-8')
 const lineReader = require('readline').createInterface({
     input: reader,
 })
-reader.on('error', (err) => console.log('hello' + err));
+reader.on('error', (err) => console.log(err));
 
 let writeStreams = [];
 
@@ -23,5 +23,7 @@ lineReader.on('line', (line) => {
     })
 })
 
-lineReader.on('end', () => console.log("Завершено чтение файла"));
+lineReader.on('close', () => console.log("Завершено чтение файла"));
 lineReader.on('error', (err) => console.log('err'));
+
+//Если есть вариант сделать через Pipe, подскажите как. У меня не получилось
